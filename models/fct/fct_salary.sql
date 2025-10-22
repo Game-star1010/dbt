aa664d66a0dd6b4a1c9d2275795a0bb353aca6d8
ns cloud <indgame9999@gmail.com>
2025-10-22 19:32:24 +0000

    fct

--- models/fct/fct_salary.sql
+++ models/fct/fct_salary.sql
@@ -1,9 +1,13 @@
 {{
     config(
-        materialized='table'
+        materialized='incremental',
+        unique_key = 'employee_id',
+        incremental_statergy = 'delete+insert',
+        tags = ['fct'],
+        schema = 'fct'
     )
 }}
-
+{% set x = "(select coalesce(max(load_time), '1900-01-01') from "~this~")" %}
 Select 
 emp.employee_id,
 jobs.job_id,
@@ -15,8 +19,9 @@ salary_date,
 HRA,
 allowances,
 PF,
-(emp.salary + HRA + allowances + PF) as gross_income,
-(emp.salary + HRA + allowances) as net_income
+to_decimal((emp.salary + HRA + allowances + PF),10,2) as gross_income,
+to_decimal((emp.salary + HRA + allowances),10,2) as net_income,
+current_timestamp as load_time
 From {{ ref('stg_salary')}} as sal
 Left join {{ ref('dim_employees')}} as emp
 on sal.employee_id = emp.employee_id
@@ -30,3 +35,24 @@ Left join {{ ref('dim_regions')}} as reg
 on con.region_id = reg.region_id
 Left join {{ ref('dim_jobs')}} as jobs
 on jobs.job_id = emp.job_id
+
+
+
+{% if is_incremental() %}
+
+where sal.load_time > {{ x }}
+
+or    emp.load_time > {{ x }}
+
+or    dept.load_time > {{ x }}
+
+or    loc.load_time > {{ x }}
+
+or    con.load_time > {{ x }}
+
+or    reg.load_time > {{ x }}
+
+or    jobs.load_time > {{ x }}
+
+{% endif %}
+
